807feb293da9d8542c412847d3871af8cf7bc6fc
Ranak Debnath <84727208+piyaldeb@users.noreply.github.com>
2024-03-16 19:20:54 +0530

    app.py

--- Server/app.py
+++ Server/app.py
@@ -75,4 +75,4 @@ def about():
     """
     return render_template("about.html", about_text=about_text)
 if __name__ == "__main__":
-    app.run(port=3010)  # Run the Flask app in debug mode for development
+    app.run(port=5010)  # Run the Flask app in debug mode for development
